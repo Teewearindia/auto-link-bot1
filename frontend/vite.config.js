87860d841600a247429d91b87e45a04be8c33c47
Anjali Chavan <teewear2250@gmail.com>
2025-04-13 07:08:50 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: './', // This ensures that the build starts from the root directory
   build: {
-    outDir: 'dist', // The build output directory
-    rollupOptions: {
-      input: './index.html', // Ensure the input file is correctly pointed to the root index.html
-    },
+    outDir: 'dist', // Output directory for build
   },
 });
