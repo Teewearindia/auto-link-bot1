ce99303b048755cc536adde16ef446cf83a0d54a
Anjali Chavan <teewear2250@gmail.com>
2025-04-13 07:41:06 +0530

    Update vite.config.js

--- frontend/src/vite.config.js
+++ frontend/src/vite.config.js
@@ -1,9 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'frontend',
   plugins: [react()],
   build: {
-    outDir: 'dist', // Output directory for build
+    outDir: 'dist',
   },
 });
