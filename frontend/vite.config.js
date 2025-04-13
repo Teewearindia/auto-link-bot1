7e7c4cd7e31fcf69c583350d2d2bfe9996c4ba99
Anjali Chavan <teewear2250@gmail.com>
2025-04-13 07:54:32 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // <-- add this
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // <-- this defines @ as /src
+    },
+  },
   build: {
-    outDir: 'dist', // Output directory for build
+    outDir: 'dist',
   },
 });
